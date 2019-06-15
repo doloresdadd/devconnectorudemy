82dcb7d819b141ba1c7b339ff2134ffaf9c2dae9
Bethanie <doloresdadd@gmail.com>
2019-06-15 16:13:21 +1000

    Add Post api

--- models/Post.js
+++ models/Post.js
@@ -16,12 +16,14 @@ const PostSchema = new Schema({
   avatar: {
     type: String
   },
-  likes: {
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: 'users'
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
     }
-  },
+  ],
   comments: [
     {
       user: {
